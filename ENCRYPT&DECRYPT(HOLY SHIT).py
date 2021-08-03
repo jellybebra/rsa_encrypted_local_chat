25fb9578f72f13e3b609aa79f3db5cb832b616cf
PeakIntegral <88369231+PeakIntegral@users.noreply.github.com>
2021-08-03 14:09:17 +0300

    Update ENCRYPT&DECRYPT(HOLY SHIT).py

--- ENCRYPT&DECRYPT(HOLY SHIT).py
+++ ENCRYPT&DECRYPT(HOLY SHIT).py
@@ -1,8 +1,9 @@
 import rsa #importim, bleyat
-#Generate keys
-(pub_key, priv_key) = rsa.newkeys(1024) #1024 - length of byts(I don't give a fuck if we need it or not)
-#enfuck & defuck
-message = b'Misha, pridumay cho s messeigem delat'
+#generate keys(degenerate my psychopass)
+(pub_key, priv_key) = rsa.newkeys(1024) #1024 - length of byts(Оказывается, там какая-то ахуенная закономерность, поэтому длина ключа должна быть больше длины самого сообщения, при том в значительное кол-во раз)
+#ENFUCK & DEFUCK
+#Я забыл как по-человечески фигачить рэйнжи, поэтому, да
+message = b'Misha, pridumay cho s messeigem delat' #эта b - если забыл - обозначает, что эта хрень имеет дело именно с байтами, без неё всё крашится к хренам
 crypto = rsa.encrypt(message, pub_key) #ENFUCK
 print(crypto)
 message = rsa.decrypt(crypto, priv_key) #DEFUCK
