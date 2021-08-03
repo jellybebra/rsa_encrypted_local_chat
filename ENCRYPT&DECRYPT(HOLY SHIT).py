1faa61dd663b13f7512723e9fab967e74e21dbfd
Mike <mihasik01rus@gmail.com>
2021-08-03 14:59:02 +0300

    RSA added

--- ENCRYPT&DECRYPT(HOLY SHIT).py
+++ ENCRYPT&DECRYPT(HOLY SHIT).py
@@ -1,11 +1,57 @@
-import rsa #importim, bleyat
-#generate keys(degenerate my psychopass)
-(pub_key, priv_key) = rsa.newkeys(1024) #1024 - length of byts(Оказывается, там какая-то ахуенная закономерность, поэтому длина ключа должна быть больше длины самого сообщения, при том в значительное кол-во раз)
-#ENFUCK & DEFUCK
-#Я забыл как по-человечески фигачить рэйнжи, поэтому, да
-message = b'Misha, pridumay cho s messeigem delat' #эта b - если забыл - обозначает, что эта хрень имеет дело именно с байтами, без неё всё крашится к хренам
-crypto = rsa.encrypt(message, pub_key) #ENFUCK
-print(crypto)
-message = rsa.decrypt(crypto, priv_key) #DEFUCK
-print(message)
+import rsa
 
+
+def gen_keys(bits=1024):
+    """
+
+    :param bits: количество бит ключа
+    :return: возвращает кортеж (открытый ключ, закрытый ключ)
+
+    Есть определенная закономерность, которая требует,
+    чтобы длина ключа была больше длины самого сообщения,
+    при том в значительное количество раз.
+
+    """
+
+    return rsa.newkeys(bits)  # генерируем ключи
+
+
+def encrypt(public_key, message):
+    """
+    Шифрует данное сообщение.
+
+    :param public_key: открытый ключ
+    :param message: сообщение
+    :return: зашифрованное сообщение
+
+    """
+    message = bytes(message, 'utf-8')
+    return rsa.encrypt(message, pub_key)
+
+
+def decrypt(private_key, message):
+    """
+    Расшифровывает данное сообщение.
+
+    :param private_key: закрытый ключ
+    :param message: побитово закодированное сообщение
+    :return: расшифрованное сообщение
+
+    """
+    message = rsa.decrypt(message, private_key)
+    return message.decode('utf-8')
+
+
+if __name__ == '__main__':
+    (pub_key, priv_key) = gen_keys()
+    print(pub_key)
+    print(priv_key)
+
+    msg = "Хуй с маслом."
+    print(f'We want to send: "{msg}"')
+
+    encrypted_msg = encrypt(pub_key, msg)
+    print(f'Encrypted message: "{encrypted_msg}"')
+
+    decrypted_msg = decrypt(priv_key, encrypted_msg)
+    print(f'Decrypted message: "{decrypted_msg}"')
